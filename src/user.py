cfda5484c1e85be0dc4424f76779ab7a411e2f1d
nemacdonald <nolanemacdonald@gmail.com>
2023-11-20 00:24:52 -0800

    Sleeper API User endpoint

---
+++ src/user.py
@@ -0,0 +1,32 @@
+from request import RequestAPI, APIException
+from logging import Logger
+import requests
+import json
+from datetime import datetime
+
+# Configure logging
+Logger.configureLog()
+
+# Get a logger for this module
+logger = Logger.getLog(__name__)
+
+
+class SleeperUser(RequestAPI):
+    def getUser(self, user):
+        """
+        Fetch user information from Sleeper API.
+
+        Args:
+            user (_type_): User ID or username
+
+        Returns:
+            user: Dictionary of user information
+        """
+        try:
+            url = f"https://api.sleeper.app/v1/user/{user}"
+            user_data = self._call(url)
+            logger.info(f"Successfully fetched user data for {user}")
+            return user_data
+        except APIException as e:
+            logger.error(f"Error fetching user data for {user}: {e}")
+            raise
